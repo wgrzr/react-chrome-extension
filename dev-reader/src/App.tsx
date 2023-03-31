a4c95a461563a17fa3a454306bd8f87a1686c441
will greer <willfromspace@gmail.com>
2023-03-30 22:44:52 -0700

    init @ wip

---
+++ dev-reader/src/App.tsx
@@ -0,0 +1,11 @@
+import ReactHtmlParser from "react-html-parser";
+
+function App() {
+  return (
+    <div className="text-gray-900 font-mono h-full w-full bg-slate-100 absolute top-0 left-0">
+      <div>inner</div>
+    </div>
+  );
+}
+
+export default App;
